1101524144d28545fd60dc516b1e3ec90d774ad8
prasannatuladhar <prasanna.pmt@gmail.com>
2020-05-15 21:47:17 +0545

    send email feature

--- plumber_site/plumber_app/views.py
+++ plumber_site/plumber_app/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 def index(request):
     return render(request,'plumber_app/index.html',{})
+
+def contact(request):
+    if request.method == "POST":
+        message_name = request.POST["message-name"]
+        message_email = request.POST["message-email"]
+        message = request.POST["message"]
+
+        send_mail('I want book a appinment',
+        message,
+        message_email,
+        ['pr_tuladhar@hotmail.com'],
+        fail_silently=False,
+        )
+        return render(request,'plumber_app/contact.html',{'message_name':message_name})    
+
+    else:
+        return render(request,'plumber_app/contact.html',{})    
